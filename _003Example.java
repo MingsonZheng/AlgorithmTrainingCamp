0fa415f271f83bba6c700805a5776733ec335955
mingsonzheng <mingsonzheng@outlook.com>
2022-05-25 15:37:18 +0800

    Example

--- _003Example.java
+++ _003Example.java
@@ -9,6 +9,32 @@ public class _003Example {
         ListNode(int val, ListNode next) { this.val = val; this.next = next; }
     }
 
+    // 例题4：剑指 Offer 25. 合并两个排序的链表（中等）
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+        ListNode p1 = l1;
+        ListNode p2 = l2;
+        ListNode head = new ListNode();// 虚拟头节点
+        ListNode tail = head;
+        while (p1 != null && p2 != null) {
+            if (p1.val <= p2.val) {
+                tail.next = p1;
+                tail = p1;
+                p1 = p1.next;
+            } else {
+                tail.next = p2;
+                tail = p2;
+                p2 = p2.next;
+            }
+        }
+        // 如果 p1 还没处理完，就把剩下的直接接到 tail 后面
+        if (p1 != null) tail.next = p1;
+        // 同理
+        if (p2 != null) tail.next = p2;
+        return head.next;
+    }
+
     // 例题3：876. 链表的中间结点
     public ListNode middleNode(ListNode head) {
         ListNode slow = head;
