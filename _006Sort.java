7d19772966aed3123cf3c6ea65762e7cba3a5949
mingsonzheng <mingsonzheng@outlook.com>
2022-06-11 10:47:27 +0800

    InsertionSort

--- _006Sort.java
+++ _006Sort.java
@@ -1,7 +1,36 @@
+import java.util.Arrays;
+
 public class _006Sort {
 
+    public static void main(String[] args) {
+        var a = new int[]{3, 8, 2, 1, 5};
+        var n = 5;
+        BubbleSort(a, n);// 冒泡排序
+        System.out.println(Arrays.toString(a));
+        a = new int[]{3, 8, 2, 1, 5};
+        InsertionSort(a, n);// 插入排序
+        System.out.println(Arrays.toString(a));
+    }
+
+    // 插入排序
+    public static void InsertionSort(int[] a, int n) {
+        if (n <= 1) return;
+        for (int i = 1; i < n; i++) {// [0, i) 已排序
+            int value = a[i];
+            int j = i - 1;
+            for (; j >= 0 ; j--) {// 查找插入的位置
+                if (a[j] > value) {
+                    a[j+1] = a[j];// 数据移动
+                } else {
+                    break;
+                }
+            }
+            a[j+1] = value;// 插入数据
+        }
+    }
+
     // 冒泡排序
-    void bubbleSort(int[] a, int n) {
+    public static void BubbleSort(int[] a, int n) {
         if (n <= 1) return;
         for (int i = 0; i < n; i++) {// 第几趟冒泡
             boolean flag = false;// 提前退出冒泡循环的标志位
