606a73dfd4e9cf9c2c2c016601fe9db73434a623
mingsonzheng <mingsonzheng@outlook.com>
2022-06-15 17:52:18 +0800

    binarySearch_r

--- _007BinarySearch.java
+++ _007BinarySearch.java
@@ -1,5 +1,22 @@
 public class _007BinarySearch {
 
+    // 二分查找代码实现--递归实现
+    public int binarySearch_r(int[] a, int n, int value) {
+        return binarySearch_r(a, 0, n-1, value);
+    }
+
+    public int binarySearch_r(int[] a, int low, int high, int value) {
+        if (low > high) return -1;
+        int mid = (low + high) / 2;
+        if (a[mid] == value) {
+            return mid;
+        } else if (a[mid] < value) {
+            return binarySearch_r(a, mid+1, high, value);
+        } else {
+            return binarySearch_r(a, low, mid-1, value);
+        }
+    }
+
     // 二分查找代码实现--非递归实现
     public int binarySearch(int[] a, int n, int value) {
         int low = 0;
