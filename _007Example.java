cbebc287541c1aca9228f1560a185dc6d9094d9f
mingsonzheng <mingsonzheng@outlook.com>
2022-06-16 15:00:02 +0800

    binarySearch_4

--- _007Example.java
+++ _007Example.java
@@ -7,6 +7,8 @@ public class _007Example {
         var a = new int[]{2, 3, 4, 4, 6, 7, 8};
         var n = 7;
         var target = 4;
+        // 查找最后一个小于等于给定值的元素
+        System.out.println(binarySearch_4(a, n, target));
         // 查找第一个大于等于给定值的元素
         System.out.println(binarySearch_3(a, n, target));
         // 查找最后一个等于给定值的元素
@@ -16,24 +18,36 @@ public class _007Example {
     }
 
     // 例题 2：查找第一个大于等于 x、最后一个小于等于 x 的元素
+    // 查找最后一个小于等于给定值的元素
+    public static int binarySearch_4(int[] a, int n, int target) {
+        int low = 0;
+        int high = n - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;// 之前的代码 mid = (low + high) / 2，但是 low + high 可能会数组越界
+            // 2, 3, 4, 4, 6, 7, 8
+            if (a[mid] <= target) {// 命中
+                // mid 是最后一个元素，或者 mid 的后一个元素不等于 target
+                if ((mid == 0) || (a[mid+1] > target)) return mid;// 真命中
+                    // mid 的后一个元素等于 target，需要查询前面的区间
+                else low = mid + 1;// 伪命中，a[mid-1] >= target
+            } else {// 不命中，a[mid] < target
+                high = mid - 1;
+            }
+        }
+        return -1;
+    }
+
     // 查找第一个大于等于给定值的元素
     public static int binarySearch_3(int[] a, int n, int target) {
         int low = 0;
         int high = n - 1;
         while (low <= high) {
             int mid = low + (high - low) / 2;// 之前的代码 mid = (low + high) / 2，但是 low + high 可能会数组越界
-
-            // 写注释让逻辑更清晰
-            // 1. a[mid] >= target
-            //   1.1 mid == 0 || a[mid-1] < target return mid;
-            //   1.2 else (a[mid+1] >= target) high = mid - 1
-            // 2. a[mid] < target high = mid + 1
-
             // 2, 3, 4, 4, 6, 7, 8
             if (a[mid] >= target) {// 命中
                 // mid 是最后一个元素，或者 mid 的后一个元素不等于 target
                 if ((mid == 0) || (a[mid-1] < target)) return mid;// 真命中
-                    // mid 的后一个元素等于 target，需要查询前面的区间
+                    // mid 的前一个元素大于等于 target，需要查询前面的区间
                 else high = mid - 1;// 伪命中，a[mid-1] >= target
             } else {// 不命中，a[mid] < target
                 low = mid + 1;
@@ -49,20 +63,12 @@ public class _007Example {
         int high = n - 1;
         while (low <= high) {
             int mid = low + (high - low) / 2;// 之前的代码 mid = (low + high) / 2，但是 low + high 可能会数组越界
-
-            // 写注释让逻辑更清晰
-            // 1. a[mid] == target
-            //   1.1 mid == n-1 || a[mid+1] != target return mid;
-            //   1.2 else (a[mid+1] == target) high = mid + 1
-            // 2. a[mid] > target low = mid + 1
-            // 3. a[mid] < target high = mid + 1
-
             // 2, 3, 4, 4, 6, 7, 8
             if (a[mid] == target) {// 命中
                 // mid 是最后一个元素，或者 mid 的后一个元素不等于 target
                 if ((mid == n-1) || (a[mid+1] != target)) return mid;// 真命中
                     // mid 的后一个元素等于 target，需要查询前面的区间
-                else high = mid + 1;// 伪命中，a[mid-1] == target
+                else low = mid + 1;// 伪命中，a[mid-1] == target
             } else if (a[mid] > target) {// 不命中
                 high = mid - 1;
             } else {// 不命中，a[mid] < target
@@ -78,14 +84,6 @@ public class _007Example {
         int high = n - 1;
         while (low <= high) {
             int mid = low + (high - low) / 2;// 之前的代码 mid = (low + high) / 2，但是 low + high 可能会数组越界
-
-            // 写注释让逻辑更清晰
-            // 1. a[mid] == target
-            //   1.1 mid == 0 || a[mid-1] != target return mid;
-            //   1.2 else (a[mid-1] == target) high = mid - 1
-            // 2. a[mid] > target low = mid + 1
-            // 3. a[mid] < target high = mid + 1
-
             // 2, 3, 4, 4, 6, 7, 8
             if (a[mid] == target) {// 命中
                 // mid 是第一个元素，或者 mid 的前一个元素不等于 target
