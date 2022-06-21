ecddfe9694dfffd61aa9926f4e08aab9fe99920f
mingsonzheng <mingsonzheng@outlook.com>
2022-06-21 17:40:14 +0800

    find

--- _009BinaryTrees/_009BinaryTrees.java
+++ _009BinaryTrees/_009BinaryTrees.java
@@ -1,16 +1,35 @@
 package _009BinaryTrees;
 
 public class _009BinaryTrees {
-    // 基于指针的存储方式（大部分情况都用这种方式）
     public class BinaryTree {
-        // 节点的定义
-        public class Node {
-            public int data;
-            public Node left;
-            public Node right;
+        // 二叉查找树的查找操作
+        // 2、非递归实现
+        public Node find(Node root, int data){
+            Node p = root;
+            while (p != null) {
+                if (data < p.data) {
+                    p = p.left;
+                } else if (data > p.data) {
+                    p = p.right;
+                } else {
+                    return p;
+                }
+            }
+            return null;
         }
 
-        private  Node root = null;
+        // 1、递归实现
+        public Node find_r(Node root, int data) {
+            if (root == null) return null;
+            if (root.data == data) {
+                return root;
+            }
+            if (data < root.data) {
+                return find_r(root.left, data);
+            } else {
+                return find_r(root.right, data);
+            }
+        }
 
         // 二叉树的遍历
         // 前序遍历：先打印根节点，然后再前序遍历左子树，最后再前序遍历右子树
@@ -34,5 +53,18 @@ public class _009BinaryTrees {
             inOrder(root.right);
             System.out.println(root.data);
         }
+
+        // 节点的定义
+        // 基于指针的存储方式（大部分情况都用这种方式）
+        public class Node {
+            public int data;
+            public Node left;
+            public Node right;
+            public Node(int data) {
+                this.data = data;
+            }
+        }
+
+        private  Node root = null;
     }
 }
