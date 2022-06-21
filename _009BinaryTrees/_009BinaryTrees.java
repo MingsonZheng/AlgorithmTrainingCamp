67009ad15160c833e20f9abea358cc21010fc73c
mingsonzheng <mingsonzheng@outlook.com>
2022-06-21 11:54:36 +0800

    BinaryTree

---
+++ _009BinaryTrees/_009BinaryTrees.java
@@ -0,0 +1,17 @@
+package _009BinaryTrees;
+
+public class _009BinaryTrees {
+    // 基于指针的存储方式（大部分情况都用这种方式）
+    public class BinaryTree {
+        // 节点的定义
+        public class Node {
+            public int data;
+            public Node left;
+            public Node right;
+        }
+
+        private  Node root = null;
+
+        // TODO: 各种操作
+    }
+}
