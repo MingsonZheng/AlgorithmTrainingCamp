9b1973bc5ff480f6ca4ef2e808b02f9175f9f20d
mingsonzheng <mingsonzheng@outlook.com>
2022-06-22 10:26:31 +0800

    insert

--- _009BinaryTrees/_009BinaryTrees.java
+++ _009BinaryTrees/_009BinaryTrees.java
@@ -2,6 +2,57 @@ package _009BinaryTrees;
 
 public class _009BinaryTrees {
     public class BinaryTree {
+
+        // 二叉查找树的插入操作
+        // 2、非递归实现
+        public void insert2(Node root, int data) {
+            if (root == null) {
+                root = new Node(data);
+                return;
+            }
+            Node p = root;
+            while (p != null) {
+                if(data > p.data) {
+                    if(p.right == null) {
+                        p.right = new Node(data);
+                        return;
+                    }
+                    p = p.right;
+                } else {
+                    if(p.left == null) {
+                        p.left = new Node(data);
+                        return;
+                    }
+                    p = p.left;
+                }
+            }
+        }
+
+        // 1、递归实现
+        public void insert(Node root, int data) {
+            if (root == null) {
+                root = new Node(data);
+                return;
+            }
+            insert_r(root, data);
+        }
+
+        public void insert_r(Node root, int data) {
+            if (data > root.data) {
+                if (root.right == null) {
+                    root.right = new Node(data);
+                } else {
+                    insert_r(root.right, data);
+                }
+            } else {
+                if (root.left == null) {
+                    root.left = new Node(data);
+                } else {
+                    insert_r(root.left, data);
+                }
+            }
+        }
+
         // 二叉查找树的查找操作
         // 2、非递归实现
         public Node find(Node root, int data){
