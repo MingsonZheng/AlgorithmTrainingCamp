f6a94a1f5fde2a6263a897c8214877f72716799d
mingsonzheng <mingsonzheng@outlook.com>
2022-07-22 00:11:40 +0800

    题型 1、二位矩阵搜索或遍历

---
+++ _013DFSAndBFS/_013Example01.java
@@ -0,0 +1,21 @@
+package _013DFSAndBFS;
+
+// 题型 1、二位矩阵搜索或遍历
+// 剑指 Offer 13. 机器人的运动范围
+
+import java.util.Arrays;
+
+public class _013Example01 {
+
+    public static void main(String[] args) {
+        System.out.println(movingCount(2,3,1));
+        System.out.println(movingCount(3,1,0));
+    }
+
+    private boolean[][] visited;
+    private int count = 0;
+
+    public static int movingCount(int m, int n, int k) {
+        return 0;
+    }
+}
