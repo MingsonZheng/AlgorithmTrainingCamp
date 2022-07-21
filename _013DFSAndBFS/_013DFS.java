5e09d264ec4783b03a1b79833a902b6d7900c227
mingsonzheng <mingsonzheng@outlook.com>
2022-07-21 23:36:38 +0800

    深度优先搜索/遍历

---
+++ _013DFSAndBFS/_013DFS.java
@@ -0,0 +1,76 @@
+package _013DFSAndBFS;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class _013DFS {
+    // 无向无权图
+    public class Graph {
+        private int v;// 顶点的个数
+        private LinkedList<Integer> adj[];// 邻接表
+
+        public Graph(int v) {
+            this.v = v;
+            adj = new LinkedList[v];
+            for (int i = 0; i < v; i++) {
+                adj[i] = new LinkedList<>();
+            }
+        }
+
+        public void addEdge(int s, int t) { // 无向图一条边存两次
+            adj[s].add(t);
+            adj[t].add(s);
+        }
+
+        boolean found = false;
+        boolean[] visited = new boolean[v];
+
+        // 是否存在
+        public boolean dfs_simple(int s, int t) {
+            dfs_simple_r(s, t);
+            return found;
+        }
+
+        private void dfs_simple_r(int s, int t) {
+            if (found) return;
+            visited[s] = true;
+            if (s == t) {
+                found = true;
+                return;
+            }
+            for (int i = 0; i < adj[s].size(); i++) {
+                int q = adj[s].get(i);
+                if (!visited[q]) {
+                    dfs_simple_r(q, t);
+                }
+            }
+        }
+
+        private List<Integer> resultPath = new ArrayList<>();
+
+        // 支持打印出从 s 到 t 的路径
+        public List<Integer> dfs(int s, int t) {
+            dfs_r(s, t, new ArrayList<>());
+            return resultPath;
+        }
+
+        public void dfs_r(int s, int t, List<Integer> path) {
+            // 结束条件
+            if (s == t) {
+                resultPath = new ArrayList<>(path);
+                return;
+            }
+
+            visited[s] = true;
+            path.add(s);
+            for (int i = 0; i < adj[s].size(); i++) {
+                int q = adj[s].get(i);
+                if (!visited[q]) {
+                    dfs_r(q, t, path);
+                }
+            }
+            path.remove(path.size()-1);// 重点理解这句，回退的时候删除（回溯）
+        }
+    }
+}
