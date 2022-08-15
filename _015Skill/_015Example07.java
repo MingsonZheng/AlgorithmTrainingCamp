2c5e457370957c6cff837e8ae170353755b7169b
mingsonzheng <mingsonzheng@outlook.com>
2022-08-15 23:44:03 +0800

    238. 除自身以外数组的乘积

--- _015Skill/_015Example07.java
+++ _015Skill/_015Example07.java
@@ -9,6 +9,7 @@ public class _015Example07 {
     public static void main(String[] args) {
         int[] nums = new int[]{1, 2, 3, 4};
         System.out.println(Arrays.toString(productExceptSelf(nums)));
+        System.out.println(Arrays.toString(productExceptSelf2(nums)));
     }
 
     public static int[] productExceptSelf(int[] nums) {
@@ -37,6 +38,26 @@ public class _015Example07 {
                 result[i] *= rightProducts[i+1];
             }
         }
+        return result;
+    }
+
+    // 用 result 替换 left
+    public static int[] productExceptSelf2(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+
+        int leftProduct = 1;
+        for (int i = 0; i < n; i++) {
+            result[i] = leftProduct;
+            leftProduct *= nums[i];
+        }
+
+        int rightProduct = 1;
+        for (int i = n-1; i >= 0; i--) {
+            result[i] *= rightProduct;
+            rightProduct *= nums[i];
+        }
+
         return result;
     }
 }
