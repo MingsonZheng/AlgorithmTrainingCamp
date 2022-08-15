91d2ab6744c622127bb72b9d251ae1a3a5ad8c5a
mingsonzheng <mingsonzheng@outlook.com>
2022-08-15 15:45:57 +0800

    283. 移动零

--- _015Skill/_015Example02.java
+++ _015Skill/_015Example02.java
@@ -2,14 +2,35 @@ package _015Skill;
 
 // 283. 移动零
 
+import java.util.Arrays;
+
 public class _015Example02 {
 
     public static void main(String[] args) {
         int[] nums = new int[]{0, 1, 0, 3, 12};
-        System.out.println(nums);
+        moveZeroes(nums);
+        System.out.println(Arrays.toString(nums));
     }
 
-    public void moveZeroes(int[] nums) {
+    public static void moveZeroes(int[] nums) {
+        int p = -1;
+        int q = 0;
+        while (q < nums.length) {
+            if (nums[q] == 0) {
+                q++;
+                continue;
+            }
+            if (nums[q] != 0) {
+                swap(nums, p+1, q);
+                p++;
+                q++;
+            }
+        }
+    }
 
+    private static void swap(int[] nums, int i, int j) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
     }
 }
